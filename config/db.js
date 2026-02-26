7002bdb95e447eb6a03de41ce6ae3545d7ca991c
Azeem Khalid <azeemkforyou@gmail.com>
2026-02-26 14:30:28 -0800

    change DNS keya ha

--- config/db.js
+++ config/db.js
@@ -1,10 +1,21 @@
-// config/db.js
 require("dotenv").config();
 const mongoose = require('mongoose');
+const dns = require('dns').promises;
+
+// Force Google DNS
+require('dns').setDefaultResultOrder('ipv4first');
+require('dns').setServers(['8.8.8.8', '1.1.1.1', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // ✅ latest Mongoose me options ki zarurat nahi
+    // SRV manually resolve karo
+    const records = await dns.resolveSrv('_mongodb._tcp.cluster0.mec1oyr.mongodb.net');
+    console.log('SRV Records:', records);
+    
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      family: 4
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
@@ -12,4 +23,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;  // 👈 ye export sahi hai
+module.exports = connectDB;
